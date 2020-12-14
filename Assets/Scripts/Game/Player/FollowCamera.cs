1862b163d3c751298fd483988cd150fb35586879
Hannes <malter2k@gmail.com>
2020-12-14 19:30:38 +0100

    Demo build WIP

--- Assets/Scripts/Game/Player/FollowCamera.cs
+++ Assets/Scripts/Game/Player/FollowCamera.cs
@@ -1,11 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pixelplacement;
 
 [ExecuteInEditMode]
 public class FollowCamera : MonoBehaviour
 {
-    [SerializeField] private Vector3 offset;
+    private static readonly float dropDistance = 50;
+    private static readonly Vector3 offset = new Vector3(-10, 10, -10);
+
+    private bool starting;
+    private bool ending;
+
+    private void OnLevelStart()
+    {
+        transform.position = Game.instance.level.omino.bottom + offset + Vector3.up * dropDistance;
+        starting = true;
+        Tween.Position(
+            target:    transform,
+            endValue:  Game.instance.level.omino.bottom + offset,
+            duration:  Constants.fadeOutTime,
+            delay:     0f,
+            easeCurve: Tween.EaseOutStrong,
+            completeCallback: () => starting = false
+        );
+    }
+
+    private void OnLevelEnd()
+    {
+        ending = true;
+        Tween.Position(
+            target:    transform,
+            endValue:  transform.position - Vector3.up * dropDistance,
+            duration:  Constants.fadeOutTime,
+            delay:     0f,
+            easeCurve: Tween.EaseInStrong,
+            completeCallback: () => ending = false
+        );
+    }
 
     private void Update()
     {
@@ -21,7 +53,11 @@ public class FollowCamera : MonoBehaviour
             else
             {
                 camera.orthographic = false;
-                transform.position = Vector3.SmoothDamp(transform.position, Game.instance.level.omino.center + offset, ref v, 0.1f);
+                if (!starting && !ending)
+                {
+                    v = default(Vector3);
+                    transform.position = Vector3.SmoothDamp(transform.position, Game.instance.level.omino.bottom + offset, ref v, 0.1f);
+                }
             }
         }
         else
