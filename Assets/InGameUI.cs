a17dfc9b1b05ab4ab8ef86128fb92cdd502a2213
Jiameng Bian <jiamengb@kth.se>
2020-12-15 03:14:41 +0800

    add ui

---
+++ Assets/InGameUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameUI : MonoBehaviour
+{
+    public Game game;
+    public Text levelText;
+
+    private int lastLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastLevel = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (game && game.levelNumber != lastLevel) {
+            levelText.text = "LEVEL " + game.levelNumber.ToString();
+        }
+    }
+}
---
+++ Assets/LevelSelect.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour
+{
+    public Sprite[] levelIcons;
+    public Level[] levels;
+    public GameObject levelImage;
+    public Text levelTitle;
+
+    private int levelIndex;
+    private int lastLevelIndex;
+    private int maxLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelIndex = 0;
+        lastLevelIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        maxLevel = levelIcons.Length;
+        if (levelImage) {
+            if (lastLevelIndex != levelIndex) {
+                lastLevelIndex = levelIndex;
+                levelImage.GetComponent<Image>().sprite = levelIcons[levelIndex];
+                int level = levelIndex + 1;
+                levelTitle.text = "LEVEL " + level.ToString();
+            }
+        }
+    }
+
+    public void IncLevel() {
+        levelIndex++;
+        levelIndex %= maxLevel;
+    }
+
+    public void DecLevel() {
+        levelIndex--;
+        if (levelIndex < 0 ) {
+            levelIndex = maxLevel - 1;
+        }
+    }
+
+    public void LoadLevelByIndex() {
+        Game[] game = FindObjectsOfType<Game>();
+        if (game.Length < 1) return;
+        game[0].LoadLevel(levelIndex + 1);
+
+    }
+
+    public void UpdateLevel() {
+        Game[] game = FindObjectsOfType<Game>();
+        if (game.Length < 1) return;
+        levelIndex = game[0].levelNumber - 1;
+        lastLevelIndex = -1; // to force update
+
+    }
+}
